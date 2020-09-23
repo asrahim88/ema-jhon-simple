0d5b0af06558afd45d4879e2e276c1f6111fa1c5
asrahim88 <tasmimhasan1997@gmail.com>
2020-09-23 16:41:43 +0600

    Something update

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,27 +3,27 @@ const Cart = (props) => {
     const cart = props.cart;
     const total = cart.reduce((total, product)=> total + product.price,0)
     const tax = total / 10;
-    let shiiping = 0;
+    let Shiping = 0;
         if (total>35) {
-            shiiping = 0;
+            Shiping = 0;
         } else if (total > 15) {
-            shiiping = 12.99
+            Shiping = 12.99
         } else if (total > 0) {
-            shiiping = 4.99;
+            Shiping = 4.99;
         }
         const formateNumber = num=>{
             const precision = num.toFixed(2);
             return Number(precision);
         }
-        const grantTotal = total + shiiping + Number(formateNumber( tax))
+        const grantTotal = total + Shiping + Number(formateNumber( tax))
     return (
         <div>
-            <h1>Order Su mmary</h1>
+            <h1>Order Summary</h1>
             <p>Item ordered: {cart.length} </p>
-            <p> <small> Shiiping Cost:</small> {shiiping}</p>
+            <p> <small> Shiping Cost:</small> {Shiping}</p>
             <p> <small>Tax + vat: {formateNumber( tax)} </small> </p>
             <p>Product price: {formateNumber(total)}</p>
-            <p>Total price: {grantTotal}</p> 
+            <p>Total price: {formateNumber(grantTotal)}</p> 
         </div>
     );
 };
